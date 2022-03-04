9a81d2dc27cc7cd3ae8ca61065d61ece5ec09c47
Gagan Yadav <gaganyadav80@gmail.com>
2022-03-04 14:51:58 +0530

    refactor: adds support for null safety

--- lib/dynamic_text_highlighting.dart
+++ lib/dynamic_text_highlighting.dart
@@ -5,27 +5,27 @@ import 'dart:math';
 
 class DynamicTextHighlighting extends StatelessWidget {
   //DynamicTextHighlighting
-  final String text;
-  final List<String> highlights;
+  final String? text;
+  final List<String>? highlights;
   final Color color;
   final TextStyle style;
   final bool caseSensitive;
 
   //RichText
   final TextAlign textAlign;
-  final TextDirection textDirection;
+  final TextDirection? textDirection;
   final bool softWrap;
   final TextOverflow overflow;
   final double textScaleFactor;
-  final int maxLines;
-  final Locale locale;
-  final StrutStyle strutStyle;
+  final int? maxLines;
+  final Locale? locale;
+  final StrutStyle? strutStyle;
   final TextWidthBasis textWidthBasis;
-  final TextHeightBehavior textHeightBehavior;
+  final TextHeightBehavior? textHeightBehavior;
 
-  DynamicTextHighlighting({
+  const DynamicTextHighlighting({
     //DynamicTextHighlighting
-    Key key,
+    Key? key,
     this.text,
     this.highlights,
     this.color = Colors.yellow,
@@ -47,86 +47,78 @@ class DynamicTextHighlighting extends StatelessWidget {
     this.textHeightBehavior,
   })  : assert(text != null),
         assert(highlights != null),
-        assert(color != null),
-        assert(style != null),
-        assert(caseSensitive != null),
-        assert(textAlign != null),
-        assert(softWrap != null),
-        assert(overflow != null),
-        assert(textScaleFactor != null),
         assert(maxLines == null || maxLines > 0),
-        assert(textWidthBasis != null),
         super(key: key);
 
   @override
   Widget build(BuildContext context) {
     //Controls
     if (text == '') {
-      return _richText(_normalSpan(text));
+      return _richText(_normalSpan(text!));
     }
-    if (highlights.isEmpty) {
-      return _richText(_normalSpan(text));
+    if (highlights!.isEmpty) {
+      return _richText(_normalSpan(text!));
     }
-    for (int i = 0; i < highlights.length; i++) {
-      if (highlights[i] == null) {
-        assert(highlights[i] != null);
-        return _richText(_normalSpan(text));
+    for (int i = 0; i < highlights!.length; i++) {
+      if (highlights?[i] == null) {
+        return _richText(_normalSpan(text!));
       }
-      if (highlights[i].isEmpty) {
-        assert(highlights[i].isNotEmpty);
-        return _richText(_normalSpan(text));
+      if (highlights![i].isEmpty) {
+        assert(highlights![i].isNotEmpty);
+        return _richText(_normalSpan(text!));
       }
     }
 
     //Main code
-    List<TextSpan> _spans = List();
+    List<TextSpan> _spans = <TextSpan>[];
     int _start = 0;
 
     //For "No Case Sensitive" option
-    String _lowerCaseText = text.toLowerCase();
-    List<String> _lowerCaseHighlights = List();
+    String _lowerCaseText = text!.toLowerCase();
+    List<String> _lowerCaseHighlights = <String>[];
 
-    highlights.forEach((element) {
+    for (var element in highlights!) {
       _lowerCaseHighlights.add(element.toLowerCase());
-    });
+    }
 
     while (true) {
-      Map<int, String> _highlightsMap = Map(); //key (index), value (highlight).
+      Map<int, String> _highlightsMap =
+          <int, String>{}; //key (index), value (highlight).
 
       if (caseSensitive) {
-        for (int i = 0; i < highlights.length; i++) {
-          int _index = text.indexOf(highlights[i], _start);
+        for (int i = 0; i < highlights!.length; i++) {
+          int _index = text!.indexOf(highlights![i], _start);
           if (_index >= 0) {
-            _highlightsMap.putIfAbsent(_index, () => highlights[i]);
+            _highlightsMap.putIfAbsent(_index, () => highlights![i]);
           }
         }
       } else {
-        for (int i = 0; i < highlights.length; i++) {
+        for (int i = 0; i < highlights!.length; i++) {
           int _index = _lowerCaseText.indexOf(_lowerCaseHighlights[i], _start);
           if (_index >= 0) {
-            _highlightsMap.putIfAbsent(_index, () => highlights[i]);
+            _highlightsMap.putIfAbsent(_index, () => highlights![i]);
           }
         }
       }
 
       if (_highlightsMap.isNotEmpty) {
-        List<int> _indexes = List();
+        List<int> _indexes = <int>[];
         _highlightsMap.forEach((key, value) => _indexes.add(key));
 
         int _currentIndex = _indexes.reduce(min);
-        String _currentHighlight = text.substring(_currentIndex,
-            _currentIndex + _highlightsMap[_currentIndex].length);
+        String _currentHighlight = text!.substring(_currentIndex,
+            _currentIndex + _highlightsMap[_currentIndex]!.length);
 
         if (_currentIndex == _start) {
           _spans.add(_highlightSpan(_currentHighlight));
           _start += _currentHighlight.length;
         } else {
-          _spans.add(_normalSpan(text.substring(_start, _currentIndex)));
+          _spans.add(_normalSpan(text!.substring(_start, _currentIndex)));
           _spans.add(_highlightSpan(_currentHighlight));
           _start = _currentIndex + _currentHighlight.length;
         }
       } else {
-        _spans.add(_normalSpan(text.substring(_start, text.length)));
+        _spans.add(_normalSpan(text!.substring(_start, text!.length)));
         break;
       }
     }
